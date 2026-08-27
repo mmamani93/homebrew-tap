5f8b9b6a7ab1bed93c964dfee2194ea8fa8e49ee
mmamani <mauro13_93@hotmail.com>
2026-08-27 14:18:18 -0300

    Update PragmAI formula to 0.7.4

--- Formula/pragmai.rb
+++ Formula/pragmai.rb
@@ -1,17 +1,17 @@
 class Pragmai < Formula
   desc "Privacy-safe local connector for PragmAI"
   homepage "https://github.com/mmamani93/pragm-ai-core"
-  version "0.7.3"
+  version "0.7.4"
   license "MPL-2.0"
 
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.3/pragmai-macos-arm64.tar.gz"
-    sha256 "a9240a68a8d65b2a98857ca756b13138a08359cebf00ab8fceb8f183ee7f3d32"
+    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.4/pragmai-macos-arm64.tar.gz"
+    sha256 "e5c29d6d53de9e54c4f4394c08ea02f609e5544b6e2f755de130077694e22b5a"
   else
-    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.3/pragmai-macos-x64.tar.gz"
-    sha256 "803d8970b85c189fd70625a6cfbdcb150697093878ef0786bdfe6133a52eb873"
+    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.4/pragmai-macos-x64.tar.gz"
+    sha256 "69aff0c9c6eccb2e65d8a38676e8a25048cdf1b42c3df0d8a14df6ec81f12ce6"
   end
 
   def install
