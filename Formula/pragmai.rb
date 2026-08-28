ed09630813099d60063a60e4369a85070ae6c584
mmamani <mauro13_93@hotmail.com>
2026-08-28 20:04:56 -0300

    Update PragmAI formula to 0.7.7

--- Formula/pragmai.rb
+++ Formula/pragmai.rb
@@ -6,11 +6,11 @@ class Pragmai < Formula
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.6/pragmai-macos-arm64.tar.gz"
-    sha256 "62ef892325bfc4bdf7e557608ba8785f920a2cb8a59866d2315d4372c0568536"
+    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.7/pragmai-macos-arm64.tar.gz"
+    sha256 "8228b0fe79087ea89de4b137019d0df2628f1ed2ac98b22cc31f16e3d9ab193b"
   else
-    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.6/pragmai-macos-x64.tar.gz"
-    sha256 "0d0dfeffebd9f4f39748306d85df5649376a05342e086277705e246d787df721"
+    url "https://github.com/mmamani93/pragm-ai-core/releases/download/v0.7.7/pragmai-macos-x64.tar.gz"
+    sha256 "835aa068b77e41c8feb78d4787bba68d4f50b66e210ef03b45a62abc89cbea54"
   end
 
   def install
